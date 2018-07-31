8bc7cf58d6c30869dd8ee866304340774b26add5
Remi-Guijarro <remi.GUIJARRO-ESPINOSA@etu.univ-amu.fr>
2018-07-31 18:49:03 +0200

    ajout d'un control du nombre de requete par jour avec le localStorage

--- automate.js
+++ automate.js
@@ -6,12 +6,12 @@ var executePrompt = () => {
             person = prompt("Please enter the number of person that you want to connect:", "20");
         } else {
             var time = prompt("Please enter the time in milisecond (1s = 1000 ms) each connection reqest will be executed every 'time' millisecond so we recommande you to put a high number to not be ban by linkedin :", "5000");
-            if(time == null || time == undefined || time == ""){
-               time = prompt("Please enter the time in milisecond (1s = 1000 ms) each connection reqest will be executed every 'time' millisecond so we recommande you to put a high number to not be ban by linkedin :", "5000");
-            }else if(time <= 0 || time < 3500){
+            if (time == null || time == undefined || time == "") {
+                time = prompt("Please enter the time in milisecond (1s = 1000 ms) each connection reqest will be executed every 'time' millisecond so we recommande you to put a high number to not be ban by linkedin :", "5000");
+            } else if (time <= 0 || time < 3500) {
                 alert('the requested interval of time is not corrrect');
                 time = prompt("Please enter the time in milisecond (1s = 1000 ms) each connection reqest will be executed every 'time' millisecond so we recommande you to put a high number to not be ban by linkedin :", "5000");
-            }else{
+            } else {
                 result = {};
                 result.time = time;
                 result.person = person
@@ -23,7 +23,7 @@ var executePrompt = () => {
 
 
 
-var invite = (NbPerson,time) => {
+var invite = (NbPerson, time) => {
     var scrollLoop = setInterval(function () {
         var connectArr = document.querySelectorAll("button[data-control-name='invite'");
         if (connectArr.length >= NbPerson) {
@@ -39,16 +39,40 @@ var invite = (NbPerson,time) => {
                 }
             }, time);
         }
-        if(connectArr.length <= 0){
-            window.scrollTo(0, document.body.scrollHeight);
-        }        
+        window.scrollTo(0, document.body.scrollHeight);
     }, 500);
 }
 
+
 var main = () => {
+    var dte = new Date();
+    let todayDate = dte.getDate();
+    if (localStorage.getItem("ladate") == undefined || localStorage.getItem("ladate") == null || localStorage.getItem("ladate") == "") {
+        localStorage.setItem("ladate", dte.getDate());
+    }
+    if (localStorage.getItem('ladate') < dte.getDate()) {
+        localStorage.setItem('nbrequete', 0);
+    }
+
+    if (localStorage.getItem('ladate') == "31") {
+        if (todayDate == 1) {
+            alert('numbers of request have been re-set to 0 for today');
+            localStorage.setItem('nbrequete', 0);
+        }
+    }
+    if (localStorage.getItem("nbrequete") == undefined || localStorage.getItem("nbrequete") == null || localStorage.getItem("nbrequete") == "") {
+        localStorage.setItem("nbrequete", 0);
+    }
     executePrompt().then((result) => {
-        invite(result.person,result.time);
+        console.log(result);
+        if (localStorage.getItem("nbrequete") >= 10) {
+            alert('you have executed' + localStorage.getItem("nbrequete") + 'today, if you don\'t want to be ban by linkedin you may stop for today')
+        } else {
+            let actualNbRequest = localStorage.getItem('nbrequete');
+            localStorage.setItem('nbrequete', actualNbRequest + result.person);
+            invite(result.person, result.time);
+        }
     })
 }
 
-main();
+main(); 
